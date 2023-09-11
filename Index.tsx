b5d27572f9f9446083f75b5827e24fd0e5b69291
aqsa-ali <mali8424124@gmail.com>
2023-09-11 09:40:08 +0500

    addes new component

--- Index.tsx
+++ Index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import App from '../App'
 
 export default function index() {
   return (
     <View>
       <Text>index</Text>
+      <App/>
     </View>
   )
 }
